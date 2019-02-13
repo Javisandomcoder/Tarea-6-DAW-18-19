67c9e06c2a4de5a171d60807d9a50a984662bbe1
Javisandom <javisandom@gmail.com>
2019-02-13 19:44:17 +0100

    Empezada la clase producto

--- Sanchez_Dominguez_Javier_Prog_Tarea06/src/libreria/Producto.java
+++ Sanchez_Dominguez_Javier_Prog_Tarea06/src/libreria/Producto.java
@@ -9,6 +9,37 @@ package libreria;
  *
  * @author javisandom
  */
-public class Producto {
+public abstract class Producto {
+    private String nombre;
+    private double precio;
+    private String descripcion;
+    public static final double MIN_PRECIO = 0.01;
+    public static final double MAX_PRECIO = 10000;
     
+    public Producto(String nombre, double precio, String descripcion)throws IllegalArgumentException{
+        if(precio <= Producto.MAX_PRECIO && precio >= Producto.MIN_PRECIO){
+            this.nombre = nombre;
+            this.precio = precio;
+            this.descripcion = descripcion;
+        }else{
+            throw new IllegalArgumentException("Error: parámetros de creación del producto invalidos. "
+                    + "El precio " + String.format("%2f", this.precio) + " no está en el rango permitido");
+        }
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+    
+    /*public String[] toArrayAtribNames(){
+        
+    }*/
 }
