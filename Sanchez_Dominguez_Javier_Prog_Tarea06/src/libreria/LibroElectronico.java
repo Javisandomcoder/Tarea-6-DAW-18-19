d37734cb15b1f41e5464f50dd8207627856cc109
Javisandom <javisandom@gmail.com>
2019-02-17 08:25:10 +0100

    Hay que terminar la clase LibroElectronico

--- Sanchez_Dominguez_Javier_Prog_Tarea06/src/libreria/LibroElectronico.java
+++ Sanchez_Dominguez_Javier_Prog_Tarea06/src/libreria/LibroElectronico.java
@@ -9,6 +9,50 @@ package libreria;
  *
  * @author javisandom
  */
-public class LibroElectronico {
+public class LibroElectronico extends Libro implements Downloadable{
+    private int tamanioArch;
+    private int descargasLibro;
+    public static final int MIN_SIZE = 20;
+    public static final int MAX_SIZE = 65535;
+
+    public LibroElectronico(String nombre, double precio, String descripcion, String autorLibro, int year, int tamanioArch, int descargasLibro) throws IllegalArgumentException {
+        super(nombre, precio, descripcion, autorLibro, year);
+        if(tamanioArch > MIN_SIZE && tamanioArch < MAX_SIZE){
+            this.tamanioArch = tamanioArch;
+            this.descargasLibro = descargasLibro;
+        }
+        else{
+            throw new IllegalArgumentException("Error: Parámetros de creación del libro electrónico inválidos"
+                    + " el tamaño (" + tamanioArch +") no está permitido" );
+        }
+    }
+    
+    public int getSize(){
+        return this.tamanioArch;
+    }
+    
+    public int getNumDescargas(){
+        return this.descargasLibro;
+    }
+
+    @Override
+    public String[] toArrayAtribValues() {
+        return super.toArrayAtribValues(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String[] toArrayAtribNames() {
+        return super.toArrayAtribNames(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    @Override
+    public double descargar(double anchoBanda) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
     
 }
